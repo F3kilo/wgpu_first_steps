4b3ad4e2d1feaac22ff213a22d534d3c7652e0c5
f3kilo <f3kilo@yandex.ru>
2022-04-02 23:11:58 +0300

    Device initialized.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use winit::dpi::PhysicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
-use winit::window::{Window, WindowBuilder};
+use winit::window::WindowBuilder;
 
 fn main() {
     // Создадим цикл обработки событий.
@@ -14,7 +14,33 @@ fn main() {
         .with_fullscreen(None)
         .with_inner_size(window_size)
         .with_title("wgpu first steps")
-        .build(&event_loop);
+        .build(&event_loop)
+        .unwrap();
+
+    // Создание экземпляра графического API.
+    let instance = wgpu::Instance::new(wgpu::Backends::all());
+
+    // Создаём поверхность для отображения.
+    let surface = unsafe { instance.create_surface(&window) };
+
+    // Задаём желаемые параметры графического устройства
+    let adapter_options = wgpu::RequestAdapterOptions {
+        compatible_surface: Some(&surface),
+        ..Default::default()
+    };
+    // Запрос адаптера - асинхронная операция.
+    let adapter_future = instance.request_adapter(&adapter_options);
+    // Дождёмся её завершения с помощью pollster.
+    let adapter = pollster::block_on(adapter_future).unwrap();
+    // Выведем в терминал название выбранного адаптера.
+    println!("Selected adapter: {}", adapter.get_info().name);
+
+    // Зададим параметры устройства по умолчанию.
+    let device_descriptor = wgpu::DeviceDescriptor::default();
+    // Запрос устройства - асинхронная операция.
+    let device_future = adapter.request_device(&device_descriptor, None);
+    // Получаем логическое устройство и очередь задач для него.
+    let (device, queue) = pollster::block_on(device_future).unwrap();
 
     // Запустим цикл обработки событий, передав в него замыкание,
     // которое будет выполнятся на кождой итерации цикла.
