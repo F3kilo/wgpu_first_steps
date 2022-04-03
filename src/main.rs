496ebea8e6a496883ee313afdf5b4095d475fa96
f3kilo <f3kilo@yandex.ru>
2022-04-03 17:42:27 +0300

    Render.

--- src/main.rs
+++ src/main.rs
@@ -56,25 +56,28 @@ fn main() {
     let surface_format = surface.get_preferred_format(&adapter).unwrap();
 
     // Зададим параметры целевого изображения. В нашем случае - поверхности в окне.
-    let color_targets = [wgpu::ColorTargetState { // Параметры цели для отрисовки.
-        format: surface_format, // Формат целевого изображения.
-        blend: None, // Смешение цветов не используем.
+    let color_targets = [wgpu::ColorTargetState {
+        // Параметры цели для отрисовки.
+        format: surface_format,         // Формат целевого изображения.
+        blend: None,                    // Смешение цветов не используем.
         write_mask: Default::default(), // Пишем во все каналы RGBA.
     }];
 
     // Параметры графического конвейера оставим, в основном, по умолчанию.
     let descriptor = wgpu::RenderPipelineDescriptor {
-        label: None, // Метку для отладки оставим не заданной.
+        label: None,                   // Метку для отладки оставим не заданной.
         primitive: Default::default(), // Создание фигур из вершин - по умолчанию.
-        vertex: wgpu::VertexState { // Параметры вершинного шейдера.
-            buffers: &[], // Буффер с данными о вершиназ не используется.
+        vertex: wgpu::VertexState {
+            // Параметры вершинного шейдера.
+            buffers: &[],           // Буффер с данными о вершиназ не используется.
             module: &shader_module, // Идентификатор нашего шейдера.
-            entry_point: "vs_main" // Имя функции, которая будет вызываться для вершин.
+            entry_point: "vs_main", // Имя функции, которая будет вызываться для вершин.
         },
-        fragment: Some(wgpu::FragmentState { // Параметры фрагментного шейдера.
+        fragment: Some(wgpu::FragmentState {
+            // Параметры фрагментного шейдера.
             targets: &color_targets, // Параметры целевого изображения.
-            module: &shader_module, // Идентификатор нашего шейдера.
-            entry_point: "fs_main", // Имя функции, которая будет вызываться для фрагментов.
+            module: &shader_module,  // Идентификатор нашего шейдера.
+            entry_point: "fs_main",  // Имя функции, которая будет вызываться для фрагментов.
         }),
         layout: None, // Разметку для передачи внешних данных в шейдер не используем.
         depth_stencil: None, // Тест глубины нам не нужен.
@@ -86,10 +89,10 @@ fn main() {
     // Настроим поверхность в соттвитствии с параметрами окна:
     let config = wgpu::SurfaceConfiguration {
         usage: wgpu::TextureUsages::RENDER_ATTACHMENT, // Будем использовать surface для рендера.
-        format: surface_format, // Формат, который мы выбрали ранее.
-        width: window_size.width, // Ширина окна.
-        height: window_size.height, // Высота окна.
-        present_mode: wgpu::PresentMode::Mailbox, // Алгоритм вывода кадров на экран.
+        format: surface_format,                        // Формат, который мы выбрали ранее.
+        width: window_size.width,                      // Ширина окна.
+        height: window_size.height,                    // Высота окна.
+        present_mode: wgpu::PresentMode::Mailbox,      // Алгоритм вывода кадров на экран.
     };
     surface.configure(&device, &config);
 
@@ -102,7 +105,50 @@ fn main() {
         match event {
             // Если обработаны все накопившиеся события - перерисовываем содержимое окна.
             Event::MainEventsCleared => {
-                // todo: код рендера
+                // Получаем следующий кадр.
+                let frame = surface.get_current_texture().unwrap();
+
+                // Создаём View для изображения этого кадра.
+                let view = frame
+                    .texture
+                    .create_view(&wgpu::TextureViewDescriptor::default());
+
+                // Создаём CommandEncoder.
+                let mut encoder =
+                    device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+                // Новая область видимости нужна, чтобы компилятор видел,
+                // что RenderPass живёт не дольше, чем CommandEncoder.
+                {
+                    let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                        label: None, // Метку для отладки оставим не заданной.
+                        color_attachments: &[wgpu::RenderPassColorAttachment {
+                            view: &view, // Цель для отрисовки.
+                            resolve_target: None, // Используется для мультисэмплинга.
+                            ops: wgpu::Operations {
+                                load: wgpu::LoadOp::Clear(wgpu::Color::BLUE), // Очищаем кадр синим цветом.
+                                store: true, // Сохраняем содержимое после завершения данного RenderPass.
+                            },
+                        }],
+                        depth_stencil_attachment: None, // Буфер глубины не используем.
+                    });
+
+                    // Задаём графический конвейер.
+                    // Все последующие операции рендеринга будут исполняться на нём.
+                    rpass.set_pipeline(&pipeline);
+
+                    // Отрисоваваем один объект с тремя вершинами.
+                    rpass.draw(0..3, 0..1);
+                }
+
+                // Сохраняем в буфер команды, записанные в CommandEncoder.
+                let command_buffer = encoder.finish();
+
+                // Передаём буфер в очередь команд устройства.
+                queue.submit(Some(command_buffer));
+
+                // Отображаем на экране отрендеренный кадр.
+                frame.present();
             }
 
             // Если было запрошено закрытие окна, завершаем цикл.
